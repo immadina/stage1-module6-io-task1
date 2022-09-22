d607c70bd1bc1cad0227212d2144c02168bb5faf
madina <91madina@gmail.com>
2022-09-22 22:32:32 +0600

    module 6, IO, task1, try 1

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,60 @@
 package com.epam.mjc.io;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        String absolutePath = "src/main/resources/Profile.txt"; //directory + File.separator + fileName;
+        String name = "n";
+        Integer age = 0;
+        String email = "e";
+        Long phone = 1111L;
+
+        /*
+        try (FileInputStream fileInputStream = new FileInputStream(absolutePath)) {
+            int ch;
+            char st;
+            while ((ch=fileInputStream.readln()) != -1) {
+                System.out.print((char)ch);
+                st += (char)ch;
+                name = Integer.toString(ch);
+
+
+            }
+        }
+        */
+
+        try (FileInputStream fis = new FileInputStream(absolutePath)) {
+
+            int i = 0;
+
+            do {
+
+                byte[] buf = new byte[1024];
+                i = fis.read(buf);
+
+                String value = new String(buf);
+                System.out.println(value);
+
+                String[] val = value.split(" ");
+                System.out.print(val);
+            } while (i != -1);
+
+
+        }
+
+
+        catch (FileNotFoundException e) {
+
+        }
+        catch (IOException e) {
+
+        }
+        return new Profile(name, age, email, phone);
     }
 }
