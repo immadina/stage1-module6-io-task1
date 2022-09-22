714384c0540b21a73b18b6de99162e574419789f
madina <91madina@gmail.com>
2022-09-23 00:23:55 +0600

    module 6, IO, task1, try 2

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -4,57 +4,57 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.HashMap;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        String absolutePath = "src/main/resources/Profile.txt"; //directory + File.separator + fileName;
+        //String absolutePath = "src/main/resources/Profile.txt"; //directory + File.separator + fileName;
         String name = "n";
         Integer age = 0;
         String email = "e";
-        Long phone = 1111L;
+        Long phone = 1L;
 
-        /*
-        try (FileInputStream fileInputStream = new FileInputStream(absolutePath)) {
-            int ch;
-            char st;
-            while ((ch=fileInputStream.readln()) != -1) {
-                System.out.print((char)ch);
-                st += (char)ch;
-                name = Integer.toString(ch);
+        FileInputStream fis = null;
+        byte[] buffer = new byte[10];
+        StringBuilder sb = new StringBuilder();
+        try {
+            fis = new FileInputStream(file);
 
+            while (fis.read(buffer) != -1) {
+                sb.append(new String(buffer));
+                buffer = new byte[10];
+            }
+            fis.close();
 
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (fis != null)
+                try {
+                    fis.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+        }
+        String st = sb.toString().replace("\n", ":").replace(" ", "");
+        String[] ar = st.split(":");
+
+        for (int i = 0; i < ar.length; i++) {
+            System.out.println(ar[i]);
+            if (ar[i].equals("Name")) {
+                name = ar[i+1];
+                System.out.println("Here");
+            } else if (ar[i].equals("Age")) {
+                age = Integer.parseInt(ar[i+1]);
+            } else if (ar[i].equals("Email")) {
+                email = ar[i+1];
+            } else if (ar[i].equals("Phone")) {
+                phone = Long.parseLong(ar[i+1]);
             }
         }
-        */
-
-        try (FileInputStream fis = new FileInputStream(absolutePath)) {
-
-            int i = 0;
-
-            do {
-
-                byte[] buf = new byte[1024];
-                i = fis.read(buf);
-
-                String value = new String(buf);
-                System.out.println(value);
-
-                String[] val = value.split(" ");
-                System.out.print(val);
-            } while (i != -1);
-
-
-        }
-
-
-        catch (FileNotFoundException e) {
-
-        }
-        catch (IOException e) {
-
-        }
+        //System.out.println(ar);
         return new Profile(name, age, email, phone);
     }
 }
