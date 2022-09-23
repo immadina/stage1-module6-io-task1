e87b8bbc2b29ed2370b45a92990f7502d92f56c2
madina <91madina@gmail.com>
2022-09-23 15:11:51 +0600

    module 6, IO, task1, try 6

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -9,52 +9,44 @@ import java.io.IOException;
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        //String absolutePath = "src/main/resources/Profile.txt"; //directory + File.separator + fileName;
-        String name = "n";
-        Integer age = 0;
-        String email = "e";
-        Long phone = 1L;
+        String name = null;
+        Integer age = null;
+        String email = null;
+        Long phone = null;
 
-        FileInputStream fis = null;
-        byte[] buffer = new byte[10];
-        StringBuilder sb = new StringBuilder();
-        try {
-            fis = new FileInputStream(file);
+        try (FileInputStream fileInputStream = new FileInputStream(file)) {
+            byte[] buffer = new byte[10];
+            StringBuilder sb = new StringBuilder();
 
-            while (fis.read(buffer) != -1) {
+            while (fileInputStream.read(buffer) != -1) {
                 sb.append(new String(buffer));
                 buffer = new byte[10];
             }
-            fis.close();
+            fileInputStream.close();
+
+            String st = sb.toString().replace("\n", ":").replace(" ", "");
+            String[] ar = st.split(":");
+
+            for (int i = 0; i < ar.length; i++) {
+
+                if (ar[i].equals("Name")) {
+                    name = ar[i + 1];
+                } else if (ar[i].equals("Age")) {
+                    age = Integer.parseInt(ar[i + 1]);
+                } else if (ar[i].equals("Email")) {
+                    email = ar[i + 1];
+                } else if (ar[i].equals("Phone")) {
+                    phone = Long.parseLong(ar[i + 1]);
+                }
+            }
 
         } catch (FileNotFoundException e) {
+            System.err.print(e);
         } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            if (fis != null)
-                try {
-                    fis.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+            System.err.print(e);
         }
-        String st = sb.toString().replace("\n", ":").replace(" ", "");
-        String[] ar = st.split(":");
 
-        for (int i = 0; i < ar.length; i++) {
-            //System.out.println(ar[i]);
-            if (ar[i].equals("Name")) {
-                name = ar[i + 1];
-                //System.out.println("Here");
-            } else if (ar[i].equals("Age")) {
-                age = Integer.parseInt(ar[i + 1]);
-            } else if (ar[i].equals("Email")) {
-                email = ar[i + 1];
-            } else if (ar[i].equals("Phone")) {
-                phone = Long.parseLong(ar[i + 1]);
-            }
-        }
-        //System.out.println(ar);
         return new Profile(name, age, email, phone);
+
     }
 }
